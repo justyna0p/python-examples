11b1fec9a7dd9526dcd21a247d5de584d1169b91
justyna0p <justyna.pedziach@gmail.com>
2021-09-14 21:50:14 +0200

    feat: math module

---
+++ Lesson-11/Lesson11.py
@@ -0,0 +1,19 @@
+import random
+
+print(random.randint(1, 10)) #biblioteka random - losuje liczby
+
+from random import randint #to samo co wyżej, ale inny sposób zapisu
+print(randint(1,10))
+
+from math import pi #importujemy nie funkcję tylko konkretną stałą
+print(pi)
+
+from math import sqrt as pierwiastek
+print(int(pierwiastek(2)))
+
+from math import ceil
+print(ceil(4.5))
+
+from math import floor
+print(floor(4.5))
+
